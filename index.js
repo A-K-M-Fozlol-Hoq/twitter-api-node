10deca5d3e87bb6ccb1b6c6cf5a7f8f20cd27c21
A-K-M-Fozlol-Hoq <akmfozlolhoq@gmail.com>
2022-10-26 08:08:31 +0600

    fixed error

--- index.js
+++ index.js
@@ -49,12 +49,12 @@ var client = new Twitter({
   access_token_key: process.env.access_token_key,
   access_token_secret: process.env.access_token_secret,
 });
-
+console.log(process.env.consumer_key_z);
 const TwitterV2 = new TwitterV2Client({
-  consumer_key: process.env.consumer_key,
-  consumer_secret: process.env.consumer_secret,
-  access_token_key: process.env.access_token_key,
-  access_token_secret: process.env.access_token_secret,
+  consumer_key: process.env.consumer_key_z,
+  consumer_secret: process.env.consumer_secret_z,
+  access_token_key: process.env.access_token_key_z,
+  access_token_secret: process.env.access_token_secret_z,
 });
 
 function extractEmails(text) {
