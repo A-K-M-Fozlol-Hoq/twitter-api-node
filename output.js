867ef3d9773838087509b191982ff373b978d60c
A-K-M-Fozlol-Hoq <akmfozlolhoq@gmail.com>
2022-11-01 12:15:05 +0600

    have to check with repid api

--- output.js
+++ output.js
@@ -1,16 +1,31 @@
 const validate = require('deep-email-validator');
 const fs = require('fs');
+const createCsvWriter = require('csv-writer').createObjectCsvWriter;
+const csvWriter = createCsvWriter({
+  path: 'check_by_csv.csv',
+  header: [{ id: 'email', title: 'EMAIL' }],
+});
+const records = [];
 
+const outputAtScrapyBird = [];
+const validEmails = [];
+const invalidEmails = [];
+let totalMatches = 0;
+fs.readFile('output.csv', 'utf8', async function (err, CSVData) {
+  const data = CSVData.split('\n');
+  data.map((email) => {
+    const e1 = email.toLocaleLowerCase().trim();
+    outputAtScrapyBird.push(e1);
+  });
+});
 fs.readFile('email.csv', 'utf8', async function (err, CSVData) {
   /* parse data */
   const data = CSVData.split('\n');
-  const validEmails = [];
-  const invalidEmails = [];
   const a = data.map(async (d) => {
     const email = d.split(',')[0];
     const screen_api = d.split(',')[1];
     let res = await validate.validate(email);
-    res.email = email;
+    res.email = email?.toLowerCase()?.trim();
     res.screen_api = screen_api;
     // console.log(res.valid);
     if (res.valid) {
@@ -28,4 +43,40 @@ fs.readFile('email.csv', 'utf8', async function (err, CSVData) {
     console.log(`${index + 1} --->`);
     console.log(email);
   });
+  execute();
 });
+
+const execute = () => {
+  validEmails.map((email, index) => {
+    const e1 = email;
+    const e2 = email.email?.toLowerCase()?.trim();
+    const e3 = email.email?.toLowerCase()?.trim();
+    console.log(
+      index,
+      '->',
+      'email.emailm-> ',
+      email.email,
+      ' validEmails.length -> ',
+      validEmails.length,
+      ' outputAtScrapyBird.includes(email) -> ',
+      outputAtScrapyBird.includes(e1),
+      ' outputAtScrapyBird.includes(email.email)-> ',
+      outputAtScrapyBird.includes(e2),
+      ' outputAtScrapyBird.length -> ',
+      outputAtScrapyBird.length
+    );
+    if (outputAtScrapyBird.includes(e3)) {
+      records.push({
+        email: e3,
+      });
+      totalMatches += 1;
+      console.log(`${index + 1} --->${email}`);
+    }
+  });
+  console.log('Total matches: ' + totalMatches);
+  csvWriter
+    .writeRecords(records) // returns a promise
+    .then(() => {
+      console.log('Done...');
+    });
+};
